5e09cf6aaba4b745548503abb0eec9ea469ea45f
niraj-scaletech <niraj.visavadiya@scaletech.xyz>
2024-11-29 12:12:09 +0530

    update: code

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@ import { logger } from "./utils";
 
 const logging = logger("scrapper");
 
-export default async function scrapeRecords(fromPage: number, toPage: number) {
-  // const from = parseInt(fromPage as string, 10);
-  // const to = parseInt(toPage as string, 10);
+export default async function scrapeRecords(fromPage: string, toPage: string) {
+  const from = parseInt(fromPage, 10);
+  const to = parseInt(toPage, 10);
 
-  if (typeof fromPage !== "number" || typeof toPage !== "number") {
+  if (typeof from !== "number" || typeof to !== "number") {
     throw new Error("please enter numbers");
   }
 
-  if (fromPage <= 0 || toPage < fromPage) {
+  if (from <= 0 || to < from) {
     throw new Error(
       "Invalid page range. Ensure 'fromPage' <= 'toPage' and both are positive integers."
     );
   }
 
-  const urls = await generatePaginationUrls(fromPage, toPage);
+  const urls = await generatePaginationUrls(from, to);
 
   await windows(urls, async (url, { page, wait, output, debug }) => {
     await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
